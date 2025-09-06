0566e749b0c433736963dcff0655abc546316780
nelfin06 <adhlirasya1@gmail.com>
2025-09-06 15:04:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Data produk untuk halaman utama
     const products = [
         { id: 'chatgpt', name: 'AI ChatGPT', category: 'produktivitas', image: 'images/chatgpt.png', status: 'Proses Manual', statusType: 'manual' },
-        { id: 'spotify', name: 'Spotify', category: 'musik-video', image: 'images/youtube.png', status: 'Beli Sekarang', statusType: 'buy' },
+        { id: 'spotify', name: 'Spotify', category: 'musik-video', image: 'images/spotify.png', status: 'Beli Sekarang', statusType: 'buy' },
         { id: 'canva', name: 'Canva', category: 'desain', image: 'images/canva.png', status: 'Proses Manual', statusType: 'manual' },
         { id: 'capcut', name: 'CapCut', category: 'desain', image: 'images/capcut.png', status: 'Beli Sekarang', statusType: 'buy' },
         { id: 'alight-motion', name: 'Alight Motion', category: 'desain', image: 'images/alightmotion.png', status: 'Beli Sekarang', statusType: 'buy' },
