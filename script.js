ea499d764a3d04d482378412aa96d0f8322abf58
nelfin06 <adhlirasya1@gmail.com>
2025-09-15 10:16:34 +0700

    update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const products = [
         { id: 'chatgpt', name: 'AI ChatGPT', category: 'produktivitas', image: 'images/chatgpt.png', status: 'Slow Delivery', statusType: 'manual' },
         
-        { id: 'spotify', name: 'Spotify', category: 'musik-video', image: 'images/spotify.png', status: 'Stok Habis', statusType: 'sold-out' },
+        { id: 'spotify', name: 'Spotify', category: 'musik-video', image: 'images/spotify.png', status: 'Fast Delivery', statusType: 'buy' },
         
         { id: 'canva', name: 'Canva', category: 'desain', image: 'images/canva.png', status: 'Slow Delivery', statusType: 'manual' },
         
-        { id: 'capcut', name: 'CapCut', category: 'desain', image: 'images/capcut.png', status: 'Stok Habis', statusType: 'sold-out' },
+        { id: 'capcut', name: 'CapCut', category: 'desain', image: 'images/capcut.png', status: 'Fast Delivery', statusType: 'buy' },
         
         { id: 'alight-motion', name: 'Alight Motion', category: 'desain', image: 'images/alightmotion.png', status: 'Fast Delivery', statusType: 'buy' },
         
