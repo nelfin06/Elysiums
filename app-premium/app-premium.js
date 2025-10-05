9dc915e4dfcfdb06185ff7b7f5f0daca0e7699ab
nelfin06 <adhlirasya1@gmail.com>
2025-10-05 13:40:25 +0700

    update app-premium/app-premium.js

--- app-premium/app-premium.js
+++ app-premium/app-premium.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const products = [
         { id: 'chatgpt', name: 'AI ChatGPT', category: 'produktivitas', image: '../../images/chatgpt.png', status: 'Stok Habis', statusType: 'sold-out' },
         
-        { id: 'spotify', name: 'Spotify', category: 'musik-video', image: '../../images/spotify.png', status: 'Fast Delivery', statusType: 'woi' },
+        { id: 'spotify', name: 'Spotify', category: 'musik-video', image: '../../images/spotify.png', status: 'Stok Habis', statusType: 'sold-out' },
         
         { id: 'canva', name: 'Canva', category: 'desain', image: '../../images/canva.png', status: 'Proses Manual', statusType: 'manual' },
         
