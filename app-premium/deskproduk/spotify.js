1487bab9d33f4a9df212da987d898f7e85d31bc5
nelfin06 <adhlirasya1@gmail.com>
2025-10-06 17:31:09 +0700

    update app-premium/deskproduk/spotify.js

--- app-premium/deskproduk/spotify.js
+++ app-premium/deskproduk/spotify.js
@@ -23,7 +23,7 @@ menuToggle.addEventListener('click', () => {
         compatibility: [ "Android", "iOS", "Semua Perangkat" ],
         terms: "Akun Private Dan Dari Admin",
         variants: [
-            { name: 'Student Premium - 1 Bulan', price: 8000, oldPrice: 16000, discount: 50 },
+            { name: 'Student Premium - 1 Bulan', price: 10000, oldPrice: 16000, discount: 37 },
         ]
     };
 
